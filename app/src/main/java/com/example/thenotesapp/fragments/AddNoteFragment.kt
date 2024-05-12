7137fcb46b3b5e949efddafe77a94c2e93c2f581
Dinithi Rajapaksha <153760230+DinithiTR@users.noreply.github.com>
2024-05-12 10:35:46 +0530

    save data correction

--- app/src/main/java/com/example/thenotesapp/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/thenotesapp/fragments/AddNoteFragment.kt
@@ -49,7 +49,7 @@ class AddNoteFragment : Fragment(R.layout.fragment_add_note), MenuProvider {
 
     private fun saveNote(view: View){
         val noteTitle = binding.addNoteTitle.text.toString().trim()
-        val noteDesc = binding.addNoteTitle.text.toString().trim()
+        val noteDesc = binding.addNoteDesc.text.toString().trim()
 
         if (noteTitle.isNotEmpty()){
             val note = Note(0,noteTitle, noteDesc)
